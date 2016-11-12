6a65184f56ec3f41c95b1bd42c9733fa7a250548
Maxim Yurkovsky <maxyor6040@gmail.com>
2016-11-12 14:59:04 +0200

    abstract_player.py: development cards implmeneted

--- game/abstract_player.py
+++ game/abstract_player.py
@@ -6,13 +6,20 @@ from game.board import Resource
 
 
 class DevelopmentCard(enum.Enum):
-    Knight = 1
-    # TODO write the rest
+    Knight = 1  # 15 cards
+    VictoryPoint = 2  # 5 cards
+    RoadBuilding = 3  # 2 cards
+    Monopoly = 4  # 2 cards
+    YearOfPlenty = 5  # 2 cards
 
 
 class AbstractPlayer(abc.ABC):
     def __init__(self):
         self.resources = {r: 0 for r in Resource}
+        self.unexposed_development_cards = {card: 0 for card in
+                                            DevelopmentCard}  # TODO make sure it's what I think it is
+        self.exposed_development_cards = {card: 0 for card in
+                                          DevelopmentCard}  # TODO make sure it's what I think it is
 
     @abc.abstractmethod
     def choose_move(self, state: AbstractState):
@@ -49,20 +56,46 @@ class AbstractPlayer(abc.ABC):
         """
         return self.resources[resource_type]
 
-    def add_development_card(self, card: DevelopmentCard):
+    def add_unexposed_development_card(self, card: DevelopmentCard):
         """
-        add a development-card to this player's development-cards
-        :param card: the development card to add
+        increase by 1 the count of the development card 'card'
+        :param card: the (probably) purchased development card
         :return: None
         """
-        pass
+        self.unexposed_development_cards[card] += 1
 
-    def remove_development_card(self, card: DevelopmentCard):
+    def remove_unexposed_development_card(self, card: DevelopmentCard):
         """
-        remove a development-card from this player's development-cards
-        :param card: the development card to remove
+        revert the side effects of 'add_unexposed_development_card' method
+        :param card: the (probably) purchased development card to be "un-purchased"
         :return: None
         """
+        self.unexposed_development_cards[card] -= 1
+
+    def expose_development_card(self, card: DevelopmentCard):
+        """
+        only counts the number of exposed/unexposed cards!
+        card effect not applied!
+        :param card: the exposed development card
+        :return: None
+        """
+        assert self.unexposed_development_cards[card] >= 1
+        self.unexposed_development_cards[card] -= 1
+        self.exposed_development_cards[card] += 1
+
+    def un_expose_development_card(self, card: DevelopmentCard):
+        """
+        only counts the number of exposed/unexposed cards!
+        card effect not reverted!
+        :param card: the exposed development card to be un-exposed
+        :return: None
+        """
+        assert self.exposed_development_cards[card] >= 1
+        self.unexposed_development_cards[card] += 1
+        self.exposed_development_cards[card] -= 1
+
+    def get_unexposed_development_cards(self):
+        # TODO implement (make sure it works with the "get next moves" implementation, don't return victory point cards)
         pass
 
     def get_exposed_knights_count(self) -> int:
@@ -70,7 +103,25 @@ class AbstractPlayer(abc.ABC):
         get the number of times this player used a "knight" development-card
         :return: int, the number of times "knight" card was used by the player
         """
-        pass
+        return self.exposed_development_cards[DevelopmentCard.Knight]
+
+    def get_victory_point_development_cards_count(self) -> int:
+        """
+        get the number of "victory points" development-card the player has
+        :return: int, the number of times "victory points" development-card the player has
+        """
+        return self.unexposed_development_cards[DevelopmentCard.VictoryPoint]
+
+    def has_unexposed_development_card(self):
+        """
+        indicate whether there is an unexposed development card
+        victory point cards are not checked - they are never exposed
+        :return: True if there is an unexposed development card, False otherwise
+        """
+        for c in DevelopmentCard:
+            if c != DevelopmentCard.VictoryPoint and self.unexposed_development_cards[c] != 0:
+                return True
+        return False
 
     def has_resources_for_road(self):
         """
