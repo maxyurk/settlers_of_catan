002701128557e5d237f18ea2d970b5f3100e6504
yurkovsk <aryeyur@campus.technion.ac.il>
2016-12-02 18:48:14 +0200

    learn_weights refactored

--- train_and_test/learn_weights.py
+++ train_and_test/learn_weights.py
@@ -1,21 +1,24 @@
 import copy
 import pickle
+from typing import Dict
 
 from algorithms.first_choice_hill_climbing import *
 from game.catan_state import CatanState
 from players.expectimax_weighted_probabilities_player import AlphaBetaWeightedProbabilitiesPlayer
 from train_and_test.logger import logger
 
+learned_weights_file_name = 'learned_weights'
+
 
 class WeightsSpace(AbstractHillClimbableSpace):
     def __init__(self):
-        self.time_seconds = 0.0005
+        self._time_seconds = 2
         self._iterations_count = 0
         self._max_iterations = 10
         self._games_per_iteration = 5
         self._seeds = [i for i in range(1, self._games_per_iteration + 1)]
-        self.unit = 0.4
-        self._delta = 3
+        self.delta_unit = 0.4
+        self._delta_is_better = 3
 
     def evaluate_state(self, weights) -> AbstractHillClimbingStateEvaluation:
         logger.info('| evaluating weights: {}'.format(weights))
@@ -23,8 +26,8 @@ class WeightsSpace(AbstractHillClimbableSpace):
 
         for i in range(self._games_per_iteration):
             seed = self._seeds[i]
-            p0 = AlphaBetaWeightedProbabilitiesPlayer(seed, self.time_seconds)
-            p1 = AlphaBetaWeightedProbabilitiesPlayer(seed, self.time_seconds, weights)
+            p0 = AlphaBetaWeightedProbabilitiesPlayer(seed, self._time_seconds)
+            p1 = AlphaBetaWeightedProbabilitiesPlayer(seed, self._time_seconds, weights)
             state = CatanState([p0, p1], seed)
 
             while not state.is_final():
@@ -32,7 +35,7 @@ class WeightsSpace(AbstractHillClimbableSpace):
                 state.make_random_move()
 
             scores = state.get_scores_by_player()
-            logger.info('| done iteration {}. scores:{}'
+            logger.info('| done iteration {}. scores: {}'
                         .format(i, {'p0 (default weights)': scores[p0], 'p1 (new weights)': scores[p1]}))
 
             evaluation += scores[p1]
@@ -44,32 +47,47 @@ class WeightsSpace(AbstractHillClimbableSpace):
 
     def get_neighbors(self, weights):
         next_weights = copy.deepcopy(weights)
-        unit_fraction = self.unit / len(weights)
+        unit_fraction = self.delta_unit / len(weights)
         for key in next_weights.keys():
             next_weights[key] -= unit_fraction
 
         for key in next_weights.keys():
-            weight_modification = self.unit
+            weight_modification = self.delta_unit
             next_weights[key] += weight_modification
             yield next_weights
             next_weights[key] -= weight_modification
 
     def is_better(self, first_victories_count: int, second_victories_count: int) -> bool:
-        logger.info('| is weight better: {}'.format(first_victories_count > second_victories_count + self._delta))
-        return first_victories_count > second_victories_count + self._delta
+        is_better = first_victories_count > second_victories_count + self._delta_is_better
+        logger.info('| is weight better: {}'.format(is_better))
+        return is_better
 
     def enough_iterations(self) -> bool:
         return self._iterations_count >= self._max_iterations
 
-if __name__ == '__main__':
+
+def dump_weights(weights):
+    f = open(learned_weights_file_name, 'wb+')
+    pickle.dump(weights, f)
+
+
+def load_weights() -> Dict[Any, float]:
+    f = open(learned_weights_file_name, 'rb+')
+    return pickle.load(f)
+
+
+def main():
     space = WeightsSpace()
     previous_result, result = None, None
-    for _ in range(5):
-        space.unit /= 2
+    for _ in range(3):
         result = first_choice_hill_climbing(space, AlphaBetaWeightedProbabilitiesPlayer.default_weights)
         if result == previous_result:
             break
         previous_result = result
+        space.delta_unit /= 2
     logger.info('| learned weights: {}'.format(result))
-    f = open('learned_weights', 'wb+')
-    pickle.dump(result, f)
+    dump_weights(result)
+
+
+if __name__ == '__main__':
+    main()
